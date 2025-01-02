8b7abe46c515d4efdb9d4c6d874402daf9319452
phamanhtuan-coder <0306221391@caothang.edu.vn>
2025-01-03 01:19:06 +0700

    update users delete

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -34,7 +34,6 @@ exports.createUser = async (req, res) => {
 
 
 // User login with token generation
-// User login with token generation
 exports.loginUser = async (req, res) => {
     try {
         const { email, password } = req.body;
@@ -108,11 +107,12 @@ exports.deleteUserById = async (req, res) => {
         if (!user) return res.status(404).json({ error: 'User not found' });
 
         await user.destroy();
-        res.status(204).send();
+        res.status(200).json({ message: 'User deleted successfully' });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 };
+
 
 // Assign role to a user
 // exports.assignRoleToUser = async (req, res) => {
