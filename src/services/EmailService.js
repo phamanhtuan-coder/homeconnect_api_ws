b4b140ce1db080837bdbdfff0db9e86b04c2f711
phamanhtuan-coder <0306221391@caothang.edu.vn>
2025-01-13 16:12:47 +0700

    Hàm xử lý gửi email cảnh báo

--- src/services/EmailService.js
+++ src/services/EmailService.js
@@ -52,6 +52,35 @@ const sendOtpEmail = async (receiverEmail, otp) => {
     }
 };
 
+/**
+ * Hàm gửi email cảnh báo khẩn cấp
+ * @param {string} receiverEmail - Địa chỉ email người nhận
+ * @param {string} alertMessage - Thông điệp cảnh báo
+ */
+const sendEmergencyAlertEmail = async (receiverEmail, alertMessage) => {
+    try {
+        const mailOptions = {
+            from: `"Team IoT - CKC F.IT SmartNet Solutions" <${process.env.EMAIL_USER}>`,
+            to: receiverEmail,
+            subject: 'CẢNH BÁO KHẨN CẤP TỪ HỆ THỐNG',
+            template: 'emergencyAlertTemplate', // Tên file template không bao gồm đuôi
+            context: {
+                message: alertMessage,
+                logoUrl:  'https://ibb.co/ZzhfQ5h', // Thay bằng URL logo của bạn
+                currentYear: new Date().getFullYear()
+            }
+        };
+
+        // Gửi email
+        await transporter.sendMail(mailOptions);
+        console.log(`Email cảnh báo khẩn cấp đã được gửi đến ${receiverEmail}: ${alertMessage}`);
+    } catch (error) {
+        console.error('Lỗi khi gửi email cảnh báo khẩn cấp:', error);
+        throw error;
+    }
+};
+
 module.exports = {
-    sendOtpEmail
+    sendOtpEmail,
+    sendEmergencyAlertEmail
 };
